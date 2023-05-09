9ea4e691920fe8b524e62268f3f24deb0558e642
Silverlan <silverlann@gmail.com>
2023-05-09 19:35:43 +0200

    feat: add localization files

---
+++ lua/autorun/client/cl_pfm_core_tutorials.lua
@@ -0,0 +1,9 @@
+--[[
+    Copyright (C) 2023 Silverlan
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+]]
+
+locale.load("pfm_core_tutorials.txt")
