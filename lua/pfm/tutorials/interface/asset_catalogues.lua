12e4f71ef4d0f438225c106037bcc6202d81b5cf
Silverlan <silverlann@gmail.com>
2023-07-16 21:34:47 +0200

    feat: finalize asset catalogues tutorial

--- lua/pfm/tutorials/interface/asset_catalogues.lua
+++ lua/pfm/tutorials/interface/asset_catalogues.lua
@@ -8,80 +8,91 @@
 
 include("/pfm/pfm_core_tutorials.lua")
 
-gui.Tutorial.register_tutorial("asset_catalogues", function(elTut, pm)
-	elTut:RegisterSlide("welcome", {
-		init = function(slideData, slide)
+gui.Tutorial.register_tutorial("asset_catalogues", "tutorials/interface/asset_catalogues", function(elTut, pm)
+	elTut:RegisterSlide("intro", {
+		init = function(tutorialData, slideData, slide)
 			slide:OpenWindow("model_catalog")
 			slide:SetMinWindowFrameDividerFraction("model_catalog", 0.5)
 			slide:SetFocusElement(slide:FindElementByPath("editor_frame/tab_button_container"))
 			slide:AddHighlight(slide:FindElementByPath("model_catalog_tab_button"))
-			slide:AddMessageBox(
-				"Welcome to the asset catalog tutorial. Asset catalogs (or explorers) list all of the available assets installed "
-					.. "in Pragma, such as models, materials or particle effects. Each of these asset types has a separate catalog, but "
-					.. "for the sake of this tutorial we will focus on the model catalog.\n\n"
-					.. 'Open the "Model Catalog" to continue.'
-			)
+			slide:AddGenericMessageBox()
 
 			-- Only auto-continue if model catalog is not already open
 			slideData.autoContinue = not pm:IsWindowActive("model_catalog")
 		end,
-		clear = function(slideData) end,
-		clearCondition = function(slideData)
+		clear = function(tutorialData, slideData) end,
+		clearCondition = function(tutorialData, slideData)
 			return pm:IsWindowActive("model_catalog")
 		end,
 		nextSlide = "overview",
 	})
 
 	elTut:RegisterSlide("overview", {
-		init = function(slideData, slide)
+		init = function(tutorialData, slideData, slide)
 			slide:GoToWindow("model_catalog")
 			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID))
 			--slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID .. "/show_external_assets"))
-			slide:AddMessageBox(
-				"Here you can find the model catalog. The catalog lists all "
-					.. "of the installed models that can be used in your project.\n"
-					.. "Models (and other asset types) are distinguished between native and external assets. "
-					.. "Native assets are assets that already exist in Pragma's own format and can be used immediately.\n\n"
-					.. "External assets are, for example, assets that were detected in installed Source Engine games, which have to be "
-					.. "imported first. The import process is fully automatic, however external assets will show as a "
-					.. "placeholder icon until they are imported. Once imported, they also count as native assets.\n\n"
-					.. 'If you only want native assets shown in the catalog, you can disable the "Show external assets" option at the top.'
-			)
+			slide:AddGenericMessageBox()
 		end,
-		clear = function(slideData) end,
-		nextSlide = "external_assets",
+		clear = function(tutorialData, slideData) end,
+		clearCondition = function(tutorialData, slideData)
+			local window = pm:GetWindow("model_catalog")
+			local explorer = util.is_valid(window) and window:GetExplorer() or nil
+			if util.is_valid(explorer) == false then
+				return true
+			end
+			return explorer:GetPath() == "pfm/demo/"
+		end,
+		nextSlide = "overview_external_placeholder",
 	})
 
-	elTut:RegisterSlide("external_assets", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("overview_external_placeholder", {
+		init = function(tutorialData, slideData, slide)
 			slide:GoToWindow("model_catalog")
-			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID))
-			--slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID .. "/show_external_assets"))
-			slide:AddMessageBox(
-				'You can import an external asset by right-clicking it and selecting "Import asset", or you can right-click '
-					.. 'the empty space between assets and select "Import all assets". This will import all of the external assets in the current directory, '
-					.. "which may require some time (and disk space).\n"
-					.. "Otherwise external assets are automatically imported whenever they are used for the first time."
+			slide:SetFocusElement(slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID))
+			local _, elOutline = slide:AddHighlight(
+				slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID .. "/models_pfm_demo_plant.glb")
 			)
+			slide:AddHighlight("context_menu/import_asset")
+			slide:SetArrowTarget(elOutline)
+			slide:AddGenericMessageBox()
 		end,
-		clear = function(slideData) end,
+		clearCondition = function(tutorialData, slideData)
+			return asset.exists("pfm/demo/plant", asset.TYPE_MODEL)
+		end,
+		clear = function(tutorialData, slideData) end,
+		nextSlide = "overview_external_placeholder2",
+	})
+
+	elTut:RegisterSlide("overview_external_placeholder2", {
+		init = function(tutorialData, slideData, slide)
+			slide:GoToWindow("model_catalog")
+			slide:SetFocusElement(slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID))
+			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID))
+			slide:AddGenericMessageBox()
+		end,
+		clear = function(tutorialData, slideData) end,
 		nextSlide = "smart_filter",
 	})
 
 	elTut:RegisterSlide("smart_filter", {
-		init = function(slideData, slide)
+		init = function(tutorialData, slideData, slide)
 			slide:GoToWindow("model_catalog")
 			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID .. "/filter"))
-			slide:AddMessageBox(
-				"You can use the smart filter to search for specific assets. The name does not have to be an exact match.\n\n"
-					.. 'Try typing "chair" into the filter field and press enter to continue.'
-			)
+			slide:AddGenericMessageBox()
 			slideData.elFilter = slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID .. "/filter")
 			local target = util.is_valid(slideData.elFilter) and slideData.elFilter:GetTarget() or nil
 			slideData.autoContinue = not (util.is_valid(target) and target:GetText():lower() == "chair")
+
+			local window = pm:GetWindow("model_catalog")
+			local explorer = util.is_valid(window) and window:GetExplorer() or nil
+			if util.is_valid(explorer) then
+				explorer:SetPath("")
+				explorer:Update()
+			end
 		end,
-		clear = function(slideData) end,
-		clearCondition = function(slideData, slide)
+		clear = function(tutorialData, slideData) end,
+		clearCondition = function(tutorialData, slideData, slide)
 			if util.is_valid(slideData.elFilter) == false then
 				return true
 			end
@@ -95,29 +106,24 @@ gui.Tutorial.register_tutorial("asset_catalogues", function(elTut, pm)
 	})
 
 	elTut:RegisterSlide("smart_filter2", {
-		init = function(slideData, slide)
+		init = function(tutorialData, slideData, slide)
 			slide:GoToWindow("model_catalog")
 			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID .. "/scroll_container"))
-			slide:AddMessageBox(
-				"The filter looks through all available assets in all directories and lists the best matches in order, starting at the top."
-			)
+			slide:AddGenericMessageBox()
 		end,
-		clear = function(slideData) end,
+		clear = function(tutorialData, slideData) end,
 		nextSlide = "drag_and_drop",
 	})
 
 	elTut:RegisterSlide("drag_and_drop", {
-		init = function(slideData, slide)
+		init = function(tutorialData, slideData, slide)
 			slide:GoToWindow("model_catalog", 0.3)
 			slide:GoToWindow("primary_viewport", 0.5)
 			slide:SetFocusElement(slide:FindElementByPath("contents"))
 			slide:AddHighlight(
 				slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID .. "/models_props_living_room_armchair")
 			)
-			slide:AddMessageBox(
-				"To place a model in your scene, you can simply drag-and-drop it from the catalog into the viewport to the right.\n\n"
-					.. "Try placing this chair somewhere in your scene."
-			)
+			slide:AddGenericMessageBox()
 			local gameView = pm:GetGameView()
 			local projectC = util.is_valid(gameView) and gameView:GetComponent(ents.COMPONENT_PFM_PROJECT) or nil
 			if projectC ~= nil then
@@ -133,99 +139,93 @@ gui.Tutorial.register_tutorial("asset_catalogues", function(elTut, pm)
 				slideData.actorWasCreated = true
 			end
 		end,
-		clear = function(slideData)
+		clear = function(tutorialData, slideData)
 			util.remove(slideData.cbOnActorCreated)
 		end,
-		clearCondition = function(slideData, slide)
+		clearCondition = function(tutorialData, slideData, slide)
 			return slideData.actorWasCreated == true
 		end,
 		nextSlide = "context_menu",
 	})
 
 	elTut:RegisterSlide("context_menu", {
-		init = function(slideData, slide)
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("model_catalog")
 			slide:AddHighlight(
 				slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID .. "/models_props_living_room_armchair")
 			)
-			slide:AddMessageBox(
-				"There are also various actions you can perform on a model by right-clicking it in the catalog, or "
-					.. "in the viewport.\n"
-					.. "You can get more information for each option through the tooltip by hovering over it, but "
-					.. "we'll go through some of the more important ones here."
-			)
+			slide:AddGenericMessageBox()
 		end,
-		clear = function(slideData) end,
+		clear = function(tutorialData, slideData) end,
 		nextSlide = "favorites",
 	})
 
 	elTut:RegisterSlide("favorites", {
-		init = function(slideData, slide)
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("model_catalog")
-			slide:AddHighlight(
+			local _, elOutline = slide:AddHighlight(
 				slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID .. "/models_props_living_room_armchair")
 			)
-			slide:AddMessageBox(
-				"If you plan on using a model frequently, you can add it to your favorites by selecting "
-					.. '"Add to favorites", after which it will appear in the "Favorites" directory in the catalog.'
-			)
+			slide:AddHighlight("context_menu/add_to_favorites")
+			slide:SetArrowTarget(elOutline)
+
+			slide:AddGenericMessageBox()
 		end,
-		clear = function(slideData) end,
+		clearCondition = function(tutorialData, slideData, slide)
+			local window = slide:GoToWindow("model_catalog")
+			local explorer = util.is_valid(window) and window:GetExplorer() or nil
+			if util.is_valid(explorer) == false then
+				return true
+			end
+			return explorer:IsInFavorites("props/living_room/armchair")
+		end,
+		clear = function(tutorialData, slideData) end,
 		nextSlide = "pack_model",
 	})
 
 	elTut:RegisterSlide("pack_model", {
-		init = function(slideData, slide)
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("model_catalog")
-			slide:AddHighlight(
+			local _, elOutline = slide:AddHighlight(
 				slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID .. "/models_props_living_room_armchair")
 			)
-			slide:AddMessageBox(
-				'You can share a model with other people, by right-clicking it and selecting "Pack Model". '
-					.. "This will create an archive with the model, as well as all materials and textures used by it. "
-					.. "To install the model, the archive can then simply be extracted into the Pragma installation directory.\n\n"
-					.. "Try packing the model now."
-			)
+			slide:AddHighlight("context_menu/pack_model")
+			slide:SetArrowTarget(elOutline)
+
+			slide:AddGenericMessageBox()
 		end,
-		clear = function(slideData) end,
+		clear = function(tutorialData, slideData) end,
 		nextSlide = "export_asset",
 	})
 
 	elTut:RegisterSlide("export_asset", {
-		init = function(slideData, slide)
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("model_catalog")
-			slide:AddHighlight(
+			local _, elOutline = slide:AddHighlight(
 				slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID .. "/models_props_living_room_armchair")
 			)
-			slide:AddMessageBox(
-				'Similarly, you can also export a model by selecting "Export asset". This will convert the model into the glTF format, '
-					.. "which can be used to then import it in a modeling software such as Blender.\n\n"
-					.. "Try exporting the model now."
-			)
+			slide:AddHighlight("context_menu/export_asset")
+			slide:SetArrowTarget(elOutline)
+			slide:AddGenericMessageBox()
 		end,
-		clear = function(slideData) end,
+		clear = function(tutorialData, slideData) end,
 		nextSlide = "asset_icons",
 	})
 
 	elTut:RegisterSlide("asset_icons", {
-		init = function(slideData, slide)
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("model_catalog")
 			slide:AddHighlight(
 				slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID .. "/models_props_living_room_armchair")
 			)
-			slide:AddMessageBox(
-				"Asset icons are automatically generated, but in some cases the default perspective may not be ideal. "
-					.. "You can change it by pressing (and holding) the right-mouse button while holding the alt-key and then "
-					.. "moving around the mouse. This also allows you to quickly inspect a model from different perspectives without "
-					.. "having to place it in the scene first."
-			)
+			slide:AddGenericMessageBox()
 		end,
-		clear = function(slideData) end,
+		clear = function(tutorialData, slideData) end,
 		nextSlide = "map_import",
 	})
 
 	elTut:RegisterSlide("map_import", {
-		init = function(slideData, slide)
+		init = function(tutorialData, slideData, slide)
 			local window = slide:GoToWindow("model_catalog")
 			local teFilter = util.is_valid(window) and window:GetFilterElement() or nil
 			if util.is_valid(teFilter) then
@@ -239,45 +239,25 @@ gui.Tutorial.register_tutorial("asset_catalogues", function(elTut, pm)
 
 			slide:SetFocusElement(slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID))
 			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_MODEL_CATALOG_UI_ID .. "/models_maps"))
-			slide:AddMessageBox(
-				"If you import a map (e.g. from the Source Engine), a model will also be generated for the map's world geometry. \n"
-					.. 'These models can be found within the "maps" directory in the map catalog and can be used just like any other '
-					.. "model. This allows you to use the geometry of multiple maps within the same PFM project."
-			)
+			slide:AddGenericMessageBox()
 		end,
-		clear = function(slideData) end,
-		nextSlide = "misc_catalogs",
-	})
-
-	elTut:RegisterSlide("misc_catalogs", {
-		init = function(slideData, slide)
-			slide:AddMessageBox(
-				"Contrary to the model catalog, the other catalog windows are not open by default. You can find the other catalogs "
-					.. 'for other asset types under "View" -> "Windows" in the menu bar.'
-			)
-		end,
-		clear = function(slideData) end,
+		clear = function(tutorialData, slideData) end,
 		nextSlide = "conclusion",
 	})
 
 	elTut:RegisterSlide("conclusion", {
-		init = function(slideData, slide)
-			slide:AddMessageBox(
-				"This concludes the tutorial on the asset catalogs.\n\n"
-					.. "You can now end the tutorial, or press the continue-button to start the next tutorial in this series."
-			)
+		init = function(tutorialData, slideData, slide)
+			slide:SetTutorialCompleted()
+			slide:AddGenericMessageBox()
 		end,
-		clear = function(slideData) end,
+		clear = function(tutorialData, slideData) end,
 		nextSlide = "next_tutorial",
 	})
 
 	elTut:RegisterSlide("next_tutorial", {
-		init = function(slideData, slide)
+		init = function(tutorialData, slideData, slide)
 			pm:LoadTutorial("interface/actor_editor")
 		end,
 	})
-
-	-- TODO: Import by drag-and-drop demo?
-
-	elTut:StartSlide("welcome")
+	elTut:StartSlide("intro")
 end)
