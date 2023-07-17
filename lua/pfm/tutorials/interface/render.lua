27f81e052377d9a1beb77fdff59fd631136a9ab5
Silverlan <silverlann@gmail.com>
2023-07-17 12:43:56 +0200

    feat: finalize rendering tutorial

--- lua/pfm/tutorials/interface/render.lua
+++ lua/pfm/tutorials/interface/render.lua
@@ -8,71 +8,54 @@
 
 include("/pfm/pfm_core_tutorials.lua")
 
--- lua_exec_cl pfm/tutorials/interface/render.lua
-
-time.create_simple_timer(0.1, function()
-	gui.Tutorial.start_tutorial("render")
-end)
-
-gui.Tutorial.register_tutorial("render", function(elTut, pm)
-	elTut:RegisterSlide("render", {
-		init = function(slideData, slide)
+gui.Tutorial.register_tutorial("render", "tutorials/interface/render", function(elTut, pm)
+	elTut:RegisterSlide("intro", {
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("render")
 			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_RENDER_UI_ID))
-			slide:AddMessageBox(
-				"Welcome to the render tutorial. This tutorial will show you how to render images and animations in the render panel."
-			)
+			slide:AddGenericMessageBox()
 		end,
-		clear = function(slideData) end,
-		nextSlide = "1",
+		clear = function(tutorialData, slideData) end,
+		nextSlide = "sections",
 	})
 
-	elTut:RegisterSlide("1", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("sections", {
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("render")
 			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_RENDER_UI_ID))
-			slide:AddMessageBox(
-				"This is the render panel. It is split into two sections:\n"
-					.. "To the left is the render preview, which is initially black because nothing has been rendered yet.\n"
-					.. "On the right you can find various controls, which allow you to change the renderer, the render resolution, etc."
-			)
+			slide:AddGenericMessageBox()
 		end,
-		clear = function(slideData) end,
-		nextSlide = "2",
+		clear = function(tutorialData, slideData) end,
+		nextSlide = "render_engine",
 	})
 
-	elTut:RegisterSlide("2", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("render_engine", {
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("render")
 			slide:AddHighlight(slide:FindElementByPath("window_render/render_engine"))
-			slide:AddMessageBox(
-				"By default there are two render engines available: Pragma and Cycles X.\n"
-					.. "The Pragma render engine simply uses Pragma's own renderer for the image. The overall render quality will be lower "
-					.. "compared to Cycles X, but rendering will be significantly faster. Pragma is recommended if you're planning on rendering animations.\n\n"
-					.. "Cycles X is a path tracing renderer, which can be used to create highly realistic images at the cost of very slow render times.\n"
-					.. "This renderer is usually a good option if you want to render a single still image.\n\n"
-					.. "For this demonstration we will be using the Cycles X renderer. Switch to it to continue."
-			)
+			slide:AddGenericMessageBox()
+
+			slideData.renderEngineField = slide:FindElementByPath(pfm.WINDOW_RENDER_UI_ID .. "/render_engine")
 		end,
-		clear = function(slideData) end,
-		nextSlide = "viewport_interaction1",
+		clear = function(tutorialData, slideData) end,
+		clearCondition = function(tutorialData, slideData)
+			local target = util.is_valid(slideData.renderEngineField) and slideData.renderEngineField:GetTarget() or nil
+			if util.is_valid(target) == false then
+				return true
+			end
+			return target:GetValue() == "cycles"
+		end,
+		nextSlide = "render_image",
 	})
 
-	elTut:RegisterSlide("3", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("render_image", {
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("render")
 			slide:AddHighlight({
 				slide:FindElementByPath("window_render/bt_render_preview"),
 				slide:FindElementByPath("window_render/bt_render_image"),
 			})
-			slide:AddMessageBox(
-				"You can use these buttons to render the image.\n"
-					.. "Rendering a preview will create a low-resolution image, which will not get saved to disk.\n"
-					.. "It's fast to render and should give you a good approximation of what the final render will look like.\n\n"
-					.. '"Render Image" will generate the full-resolution image. Depending on the project, your hardware and the render settings, '
-					.. "this can take several minutes or more.\n\n"
-					.. "Try rendering the full-resolution image now. Please note that Pragma may appear frozen for a bit if this is the first render for this project session."
-			)
+			slide:AddGenericMessageBox()
 
 			slideData.renderStarted = false
 			local elWindowRender = slide:FindElementByPath("window_render")
@@ -89,30 +72,25 @@ gui.Tutorial.register_tutorial("render", function(elTut, pm)
 				)
 			end
 		end,
-		clearCondition = function(slideData)
+		clearCondition = function(tutorialData, slideData)
 			if util.is_valid(slideData.elRenderWindow) == false then
 				return true
 			end
 			return slideData.renderStarted
 		end,
-		clear = function(slideData)
+		clear = function(tutorialData, slideData)
 			util.remove(slideData.cbOnRenderImage)
 		end,
 		autoContinue = true,
-		nextSlide = "4",
+		nextSlide = "render_progress",
 	})
 
-	elTut:RegisterSlide("4", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("render_progress", {
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("render")
 			slide:SetFocusElement(pm)
 			slide:AddHighlight(slide:FindElementByPath("info_bar/icon_container"))
-			slide:AddMessageBox(
-				"When rendering using an offline renderer like Cycles X, you can view the render progress down here.\n"
-					.. "During a render, PFM may react slowly to inputs. This is because PFM's FPS is intentionally lowered "
-					.. "to open up additional GPU cycles for the renderer.\n\n"
-					.. "Please wait for the render to complete before continuing."
-			)
+			slide:AddGenericMessageBox()
 
 			slideData.renderCompleted = false
 			local elWindowRender = slide:FindElementByPath("window_render")
@@ -126,40 +104,32 @@ gui.Tutorial.register_tutorial("render", function(elTut, pm)
 				)
 			end
 		end,
-		clearCondition = function(slideData)
+		clearCondition = function(tutorialData, slideData)
 			if util.is_valid(slideData.elRenderWindow) == false then
 				return true
 			end
 			return slideData.renderCompleted
 		end,
-		clear = function(slideData)
+		clear = function(tutorialData, slideData)
 			util.remove(slideData.cbOnRenderComplete)
 		end,
-		nextSlide = "5",
+		nextSlide = "saved_render",
 	})
 
-	elTut:RegisterSlide("5", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("saved_render", {
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("render")
 			slide:AddHighlight(slide:FindElementByPath("window_render/bt_open_output_dir"))
-			slide:AddMessageBox(
-				"The image is automatically saved to the disk. You can press this button to reveal it in the file explorer."
-			)
+			slide:AddGenericMessageBox()
 		end,
-		nextSlide = "6",
+		nextSlide = "render_job",
 	})
 
-	elTut:RegisterSlide("6", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("render_job", {
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("render")
 			slide:AddHighlight(slide:FindElementByPath("window_render/bt_create_render_job"))
-			slide:AddMessageBox(
-				'Alternatively to rendering within PFM, you can also create a "Render Job". This will create a render file, which can be used '
-					.. "to render the image without having to keep PFM running.\n"
-					.. "In some cases this can be significantly faster compared to rendering within PFM, because it allows the renderer to use "
-					.. "all available GPU resources, some of which would otherwise be taken up by PFM.\n\n"
-					.. "Try creating a render job now."
-			)
+			slide:AddGenericMessageBox()
 
 			slideData.renderJobCreated = false
 			local elWindowRender = slide:FindElementByPath("window_render")
@@ -176,65 +146,65 @@ gui.Tutorial.register_tutorial("render", function(elTut, pm)
 				)
 			end
 		end,
-		clearCondition = function(slideData)
+		clearCondition = function(tutorialData, slideData)
 			if util.is_valid(slideData.elRenderWindow) == false then
 				return true
 			end
 			return slideData.renderJobCreated
 		end,
-		clear = function(slideData)
+		clear = function(tutorialData, slideData)
 			util.remove(slideData.cbOnRenderImage)
 		end,
 		autoContinue = true,
-		nextSlide = "7",
+		nextSlide = "render_job_script",
 	})
 
-	elTut:RegisterSlide("7", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("render_job_script", {
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("render")
-			slide:AddHighlight(slide:FindElementByPath("window_render/bt_open_output_dir"))
 			local renderFileName = "render"
 			if os.SYSTEM_WINDOWS then
 				renderFileName = renderFileName .. ".bat"
 			else
 				renderFileName = renderFileName .. ".sh"
 			end
-			slide:AddMessageBox(
-				'A file explorer window should now have appeared, which contains a "'
-					.. renderFileName
-					.. '". '
-					.. "Executing this file will start the render process, and the image will be saved in the same directory once complete.\n"
-					.. "You can also create a render job for a sequence of images, in which case it will render them all one after another.\n\n"
-					.. "Render jobs are only available for offline renderers like Cycles X, and cannot be used for the Pragma renderer."
-			)
+			slide:AddGenericMessageBox({
+				renderFileName,
+			})
 		end,
-		nextSlide = "8",
+		nextSlide = "vr_render",
 	})
 
-	elTut:RegisterSlide("8", {
-		init = function(slideData, slide)
+	-- TODO: We should cover color transforms
+
+	elTut:RegisterSlide("vr_render", {
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("render")
 			slide:AddHighlight({
 				slide:FindElementByPath("window_render/preset"),
 				slide:FindElementByPath("window_render/render_engine"),
 			})
-			slide:AddMessageBox(
-				"You can also use PFM to render other image types, such as equirectangular images for VR.\n\n"
-					.. 'Change the preset to "VR" and the render engine to "Pragma" to continue.'
-			)
+			slide:AddGenericMessageBox()
+
+			slideData.renderEngineField = slide:FindElementByPath(pfm.WINDOW_RENDER_UI_ID .. "/render_engine")
+			slideData.presetField = slide:FindElementByPath(pfm.WINDOW_RENDER_UI_ID .. "/preset")
 		end,
-		nextSlide = "9",
+		clearCondition = function(tutorialData, slideData)
+			local target = util.is_valid(slideData.renderEngineField) and slideData.renderEngineField:GetTarget() or nil
+			local targetPreset = util.is_valid(slideData.presetField) and slideData.presetField:GetTarget() or nil
+			if util.is_valid(target) == false or util.is_valid(targetPreset) == false then
+				return true
+			end
+			return target:GetValue() == "pragma" and targetPreset:GetValue() == "vr"
+		end,
+		nextSlide = "vr_render_start",
 	})
 
-	elTut:RegisterSlide("9", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("vr_render_start", {
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("render")
 			slide:AddHighlight(slide:FindElementByPath("window_render/bt_render_image"))
-			slide:AddMessageBox(
-				'Now press the "Render Image" button to start the render. To get the full VR experience '
-					.. "the scene will have to be rendered twice, once for the left eye and once for the right eye.\n"
-					.. "The resulting image will then be stitched together into one equirectangular image."
-			)
+			slide:AddGenericMessageBox()
 
 			slideData.renderStarted = false
 			local elWindowRender = slide:FindElementByPath("window_render")
@@ -251,69 +221,58 @@ gui.Tutorial.register_tutorial("render", function(elTut, pm)
 				)
 			end
 		end,
-		clearCondition = function(slideData)
+		clearCondition = function(tutorialData, slideData)
 			if util.is_valid(slideData.elRenderWindow) == false then
 				return true
 			end
 			return slideData.renderStarted
 		end,
-		clear = function(slideData)
+		clear = function(tutorialData, slideData)
 			util.remove(slideData.cbOnRenderImage)
 		end,
 		autoContinue = true,
-		nextSlide = "10",
+		nextSlide = "vr_view",
 	})
 
-	elTut:RegisterSlide("10", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("vr_view", {
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("render")
 			slide:AddHighlight(slide:FindElementByPath("window_render/render_viewport"))
-			slide:AddMessageBox(
-				"You can click and drag the left mouse button in the rendered image to "
-					.. "look around as a 360 degree view."
-			)
+			slide:AddGenericMessageBox()
 		end,
-		nextSlide = "11",
+		nextSlide = "vr_view_mode",
 	})
 
-	elTut:RegisterSlide("11", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("vr_view_mode", {
+		init = function(tutorialData, slideData, slide)
 			tool.get_filmmaker():GoToWindow("render")
 			slide:SetFocusElement(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
 			slide:AddHighlight(slide:FindElementByPath("window_render/preview_mode"))
-			slide:AddMessageBox(
-				'You can toggle between the left and right eye view for the preview viewport, or select "Flat" '
-					.. "to view the actual image."
-			)
+			slide:AddGenericMessageBox()
 		end,
-		nextSlide = "12",
+		nextSlide = "vr_video",
 	})
 
-	elTut:RegisterSlide("12", {
-		init = function(slideData, slide)
-			slide:AddMessageBox(
-				"PFM can only render image sequences, so you will still need a video editing software capable of generating VR videos."
-			)
+	elTut:RegisterSlide("vr_video", {
+		init = function(tutorialData, slideData, slide)
+			slide:AddGenericMessageBox()
 		end,
-		nextSlide = "13",
+		nextSlide = "conclusion",
 	})
 
-	elTut:RegisterSlide("13", {
-		init = function(slideData, slide)
-			slide:AddMessageBox(
-				"This concludes the tutorial on the render panel.\n\n"
-					.. "You can now end the tutorial, or press the continue-button to start the next tutorial in this series."
-			)
+	elTut:RegisterSlide("conclusion", {
+		init = function(tutorialData, slideData, slide)
+			slide:SetTutorialCompleted()
+			slide:AddGenericMessageBox()
 		end,
 		nextSlide = "viewport_next_tutorial",
 	})
-	elTut:StartSlide("10")
 
 	elTut:RegisterSlide("viewport_next_tutorial", {
-		init = function(slideData, slide)
+		init = function(tutorialData, slideData, slide)
 			pm:LoadTutorial("interface/render")
 		end,
 	})
 
-	--elTut:StartSlide("viewport_fin")
+	elTut:StartSlide("intro")
 end)
