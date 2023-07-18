387f8b5a3c37829e5c906d7a3faec068c3dd9a65
Silverlan <silverlann@gmail.com>
2023-07-18 21:26:31 +0200

    feat: implement viewport tutorial (wip)

--- lua/pfm/tutorials/interface/viewport.lua
+++ lua/pfm/tutorials/interface/viewport.lua
@@ -8,8 +8,8 @@
 
 include("/pfm/pfm_core_tutorials.lua")
 
-local uuidCat = "0657005c-16f1-4ee0-a6ea-5be960267a32"
-local catTargetLocation = Vector(-133, 14, -168)
+local uuidChar = "fde723dd-1ba0-4d3e-8536-e576fc161f80"
+local charTargetLocation = Vector(-80.3301, 22.377, -164.07)
 local nextTutorial = "tutorials/interface/02_actor_editor"
 
 -- lua_exec_cl pfm/tutorials/interface/viewport.lua
@@ -18,55 +18,51 @@ time.create_simple_timer(0.1, function()
 	gui.Tutorial.start_tutorial("viewport")
 end)
 
-gui.Tutorial.register_tutorial("viewport", function(elTut, pm)
-	elTut:RegisterSlide("viewport", {
-		init = function(slideData, slide)
-			tool.get_filmmaker():GoToWindow("primary_viewport")
+gui.Tutorial.register_tutorial("viewport", "tutorials/interface/viewport", function(elTut, pm)
+	elTut:RegisterSlide("intro", {
+		init = function(tutorialData, slideData, slide)
+			slide:GoToWindow("primary_viewport")
 			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
-			slide:AddMessageBox(
-				"Welcome to the viewport tutorial. This tutorial will introduce you to the viewport, "
-					.. "where you can preview your scene and animation, move actors around, etc."
-			)
+			slide:AddGenericMessageBox()
 		end,
-		clear = function(slideData) end,
-		nextSlide = "viewport_interaction1",
+		clear = function(tutorialData, slideData) end,
+		nextSlide = "interaction1",
 	})
 
 	--[[elTut:RegisterSlide("viewport", {
-		init = function(slideData, slide)
-			tool.get_filmmaker():GoToWindow("primary_viewport")
+		init = function(tutorialData,slideData, slide)
+			slide:GoToWindow("primary_viewport")
 			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
 			slide:AddMessageBox(
 				'This is the viewport, where you can preview your scene/animation, select and transform actors, etc.\nYou can also create additional viewports from the "Windows" sub-menu in the menu bar.',
 				"pfm/tutorials/intro/viewport_test.mp3"
 			)
 		end,
-		clear = function(slideData) end,
+		clear = function(tutorialData,slideData) end,
 		nextSlide = "viewport_interaction1",
 	})]]
+	-- TODO: Secondary viewport?
 
-	elTut:RegisterSlide("viewport_interaction1", {
-		init = function(slideData, slide)
-			slide:AddHighlight(slide:FindElementByPath("pfm_primary_viewport/viewport"))
-			slide:AddMessageBox(
-				"We'll start with the camera controls. To control the viewport camera, press and hold the right mouse button, which will enable FPS controls. "
-					.. "As long as you hold down the button, "
-					.. "you can rotate the camera by moving the mouse, and move the camera using WASD. You can also change the field of view using the scroll wheel.\n\n"
-					.. "Try moving around in the scene until you find the origami cat in the dining room."
-			)
+	elTut:RegisterSlide("interaction1", {
+		init = function(tutorialData, slideData, slide)
+			slide:AddHighlight(slide:FindElementByPath("window_primary_viewport/viewport"))
+			slide:AddGenericMessageBox()
 
-			slideData.targetEntity = ents.find_by_uuid(uuidCat)
+			slideData.targetEntity = ents.find_by_uuid(uuidChar)
 			if util.is_valid(slideData.targetEntity) == false then
 				return
 			end
 			local el = gui.create("WIGameObjectSelectionOutline")
-			el:SetTarget(slide:FindElementByPath("pfm_primary_viewport/viewport"), slideData.targetEntity)
+			local elTgt = slide:FindElementByPath("window_primary_viewport/viewport", false)
+			if elTgt ~= nil then
+				el:SetTarget(elTgt, slideData.targetEntity)
+			end
 			slideData.gameObjectOutline = el
 		end,
-		clear = function(slideData)
+		clear = function(tutorialData, slideData)
 			util.remove(slideData.gameObjectOutline)
 		end,
-		clearCondition = function(slideData)
+		clearCondition = function(tutorialData, slideData)
 			if util.is_valid(slideData.gameObjectOutline) == false then
 				return true
 			end
@@ -83,200 +79,236 @@ gui.Tutorial.register_tutorial("viewport", function(elTut, pm)
 			end
 			return trC:GetDistance(slideData.targetEntity) < 100
 		end,
-		nextSlide = "viewport_interaction2",
+		nextSlide = "interaction2",
 	})
 
-	elTut:RegisterSlide("viewport_interaction2", {
-		init = function(slideData, slide)
-			slide:AddHighlight(slide:FindElementByPath("pfm_primary_viewport/viewport"))
-			slide:AddMessageBox(
-				"Now click the cat with your left mouse button to select it. Selected objects have a yellow outline around them.\n"
-					.. "You can only select objects that are part the project, map-objects, like the chairs in this scene, cannot be selected."
-			)
-			slideData.targetEntity = ents.find_by_uuid(uuidCat)
+	elTut:RegisterSlide("interaction2", {
+		init = function(tutorialData, slideData, slide)
+			slide:AddHighlight(slide:FindElementByPath("window_primary_viewport/viewport"))
+			slide:AddGenericMessageBox()
+			slideData.targetEntity = ents.find_by_uuid(uuidChar)
 			pm:DeselectAllActors()
 		end,
-		clearCondition = function(slideData)
+		clearCondition = function(tutorialData, slideData)
 			if util.is_valid(slideData.targetEntity) == false then
 				return true
 			end
 			local selC = slideData.targetEntity:GetComponent("pfm_selection_wireframe")
 			return (selC ~= nil and selC:IsPersistent())
 		end,
-		nextSlide = "viewport_manip_controls",
+		nextSlide = "interaction3",
 	})
 
-	elTut:RegisterSlide("viewport_manip_controls", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("interaction3", {
+		init = function(tutorialData, slideData, slide)
+			slide:AddHighlight(slide:FindElementByPath("window_primary_viewport/viewport"))
+			slide:AddGenericMessageBox()
+		end,
+		nextSlide = "manip_controls",
+	})
+
+	elTut:RegisterSlide("manip_controls", {
+		init = function(tutorialData, slideData, slide)
 			slide:SetFocusElement(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
-			slide:AddHighlight(slide:FindElementByPath("pfm_primary_viewport/manip_controls"))
-			slide:AddMessageBox(
-				"These are the manipulator controls, which can be used to manipulate selected actors, bones, etc. in the scene. From left to right:\n- Select ("
-					.. pfm.get_key_binding("pfm_action transform select")
-					.. ")\n- Move ("
-					.. pfm.get_key_binding("pfm_action transform translate")
-					.. ")\n- Rotate ("
-					.. pfm.get_key_binding("pfm_action transform rotate")
-					.. ")\n- Scale ("
-					.. pfm.get_key_binding("pfm_action transform scale")
-					.. ')\n\nClick on the "Move" manipulator to continue.'
-			)
+			slide:AddHighlight(slide:FindElementByPath("window_primary_viewport/manip_controls"))
+			slide:AddGenericMessageBox( --[[{
+				pfm.get_key_binding("pfm_action transform select"),
+				pfm.get_key_binding("pfm_action transform translate"),
+				pfm.get_key_binding("pfm_action transform rotate"),
+				pfm.get_key_binding("pfm_action transform scale"),
+			}]])
 
 			local vp = tool.get_filmmaker():GetViewport()
 			if util.is_valid(vp) then
 				vp:SetManipulatorMode(gui.PFMViewport.MANIPULATOR_MODE_SELECT)
 			end
+
+			local actor = ents.find_by_uuid(uuidChar)
+			local actorC = (actor ~= nil) and actor:GetComponent(ents.COMPONENT_PFM_ACTOR) or nil
+			local actorData = (actorC ~= nil) and actorC:GetActorData() or nil
+			pm:DeselectAllActors()
+			if actorData ~= nil then
+				pm:SelectActor(actorData, true)
+			end
 		end,
-		clearCondition = function(slideData)
+		clearCondition = function(tutorialData, slideData)
 			local vp = tool.get_filmmaker():GetViewport()
 			if util.is_valid(vp) == false then
 				return true
 			end
 			return vp:GetManipulatorMode() == gui.PFMViewport.MANIPULATOR_MODE_MOVE
 		end,
-		nextSlide = "viewport_move_actor",
+		nextSlide = "move_actor",
 	})
 
-	elTut:RegisterSlide("viewport_move_actor", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("move_actor", {
+		init = function(tutorialData, slideData, slide)
 			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
-			slide:AddMessageBox(
-				"Now try moving the actor into the marked spot on the table in the living room using the transform gizmo."
-			)
+			slide:AddGenericMessageBox()
 
-			slideData.targetEntity = ents.find_by_uuid(uuidCat)
+			slideData.targetEntity = ents.find_by_uuid(uuidChar)
 			if util.is_valid(slideData.targetEntity) == false then
 				return
 			end
-			slideData.locationMarker = slide:AddLocationMarker(catTargetLocation)
-		end,
-		clearCondition = function(slideData)
-			if util.is_valid(slideData.targetEntity) == false or util.is_valid(slideData.locationMarker) == false then
-				return true
-			end
-			local posMarker = slideData.locationMarker:GetPos()
-			local posEnt = slideData.targetEntity:GetPos()
-			local yDist = math.abs(posMarker.y - posEnt.y)
-			posMarker.y = 0.0
-			posEnt.y = 0.0
-			local xzDist = posMarker:Distance(posEnt)
+			slideData.locationMarker = slide:AddLocationMarker(charTargetLocation)
 
-			return yDist < 2.0 and xzDist < 8.0
+			local targetInfo = slide:AddViewportTarget(charTargetLocation, function()
+				local ent = ents.find_by_uuid(uuidChar)
+				local animC = (ent ~= nil) and ent:GetComponent(ents.COMPONENT_ANIMATED) or nil
+				if animC == nil then
+					return Vector()
+				end
+				local pose = animC:GetGlobalBonePose("lowerBody")
+				return (pose ~= nil) and pose:GetOrigin() or Vector()
+			end, 4.0)
+			if util.is_valid(targetInfo.lineEntity) then
+				targetInfo.lineEntity:SetColor(Color.Red)
+			end
+			slideData.targetInfo = targetInfo
 		end,
-		clear = function(slideData)
+		clearCondition = function(tutorialData, slideData, slide)
+			return slideData.targetInfo.isInRange()
+		end,
+		clear = function(tutorialData, slideData)
 			util.remove(slideData.locationMarker)
 		end,
-		nextSlide = "viewport_manip_controls_fin",
+		nextSlide = "rotate_actor",
 	})
 
-	elTut:RegisterSlide("viewport_manip_controls_fin", {
-		init = function(slideData, slide)
-			slide:SetFocusElement(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
-			slide:AddHighlight(slide:FindElementByPath("pfm_primary_viewport/manip_controls"))
-			slide:AddMessageBox(
-				"You can toggle the transform space between world, local and screen space by pressing the transform button multiple times."
-			)
+	elTut:RegisterSlide("rotate_actor", {
+		init = function(tutorialData, slideData, slide)
+			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
+			slide:AddGenericMessageBox()
+
+			slideData.targetEntity = ents.find_by_uuid(uuidChar)
+			if util.is_valid(slideData.targetEntity) == false then
+				return
+			end
 		end,
-		nextSlide = "viewport_transform_space",
+		clearCondition = function(tutorialData, slideData, slide)
+			if util.is_valid(slideData.targetEntity) == false then
+				return true
+			end
+			local ang = slideData.targetEntity:GetAngles()
+			return ang.y >= -105 and ang.y <= -75
+		end,
+		nextSlide = "manip_controls_fin",
 	})
 
-	elTut:RegisterSlide("viewport_transform_space", {
-		init = function(slideData, slide)
+	-- TODO: This should include a short explanation of what "transform space" actually is
+	elTut:RegisterSlide("manip_controls_fin", {
+		init = function(tutorialData, slideData, slide)
 			slide:SetFocusElement(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
-			slide:AddHighlight(slide:FindElementByPath("pfm_primary_viewport/vp_settings/transform_space"))
-			slide:AddMessageBox("Alternatively you can also change the transform space over here.")
+			slide:AddHighlight(slide:FindElementByPath("window_primary_viewport/manip_controls"))
+			slide:AddGenericMessageBox()
+		end,
+		nextSlide = "transform_space",
+	})
+
+	elTut:RegisterSlide("transform_space", {
+		init = function(tutorialData, slideData, slide)
+			slide:SetFocusElement(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
+			slide:AddHighlight(slide:FindElementByPath("window_primary_viewport/vp_settings/transform_space"))
+			slide:AddGenericMessageBox()
 		end,
 		nextSlide = "snap_to_grid",
 	})
 
 	elTut:RegisterSlide("snap_to_grid", {
-		init = function(slideData, slide)
+		init = function(tutorialData, slideData, slide)
 			slide:SetFocusElement(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
 			slide:AddHighlight({
-				slide:FindElementByPath("pfm_primary_viewport/vp_settings/snap_to_grid_spacing"),
-				slide:FindElementByPath("pfm_primary_viewport/vp_settings/angular_spacing"),
+				slide:FindElementByPath("window_primary_viewport/vp_settings/snap_to_grid_spacing"),
+				slide:FindElementByPath("window_primary_viewport/vp_settings/angular_spacing"),
 			})
-			slide:AddMessageBox("You can also enable snap-to-grid to make it easier to align objects.")
+			slide:AddGenericMessageBox()
 		end,
-		nextSlide = "viewport_live_render",
+		nextSlide = "live_render",
 	})
 
-	elTut:RegisterSlide("viewport_live_render", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("live_render", {
+		init = function(tutorialData, slideData, slide)
 			slide:SetFocusElement(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
-			slide:AddHighlight(slide:FindElementByPath("pfm_primary_viewport/vp_settings/rt_enabled"))
-			slide:AddMessageBox(
-				'The "Live Render" setting toggles the viewport to a real-time raytraced preview of the scene, rendered using the Cycles renderer. This can be useful to get a quick preview of what your scene will look like with baked lightmaps, or when using the Cycles renderer to render the final image.\nYou can still move around the scene with the "Live Render" mode active, but some actions (like adding or removing actors) will not be visible until the mode is deactivated.'
-			)
+			slide:AddHighlight(slide:FindElementByPath("window_primary_viewport/vp_settings/rt_enabled"))
+			slide:AddGenericMessageBox()
+		end,
+		clearCondition = function(tutorialData, slideData, slide)
+			local el = slide:FindElementByPath("window_primary_viewport/vp_settings/rt_enabled", false)
+			local tgt = util.is_valid(el) and el:GetTarget() or nil
+			if util.is_valid(tgt) == false then
+				return true
+			end
+			return toboolean(tgt:GetValue())
+		end,
+		autoContinue = true,
+		nextSlide = "live_render_active",
+	})
+
+	elTut:RegisterSlide("live_render_active", {
+		init = function(tutorialData, slideData, slide)
+			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
+			slide:AddGenericMessageBox()
 		end,
 		nextSlide = "cc_controls",
 	})
 
 	elTut:RegisterSlide("cc_controls", {
-		init = function(slideData, slide)
+		init = function(tutorialData, slideData, slide)
+			local el = slide:FindElementByPath("window_primary_viewport/vp_settings/rt_enabled", false)
+			local tgt = util.is_valid(el) and el:GetTarget() or nil
+			if util.is_valid(tgt) then
+				tgt:SelectOption(0)
+			end
+
 			slide:SetFocusElement(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
-			slide:AddHighlight(slide:FindElementByPath("pfm_primary_viewport/cc_controls"))
-			slide:AddMessageBox(
-				"These are the camera controls. Here you toggle between the work and scene camera, or change the scene camera to a different one.\n\nSwitch to the scene camera to continue."
-			)
+			slide:AddHighlight(slide:FindElementByPath("window_primary_viewport/cc_controls"))
+			slide:AddGenericMessageBox()
 		end,
-		clearCondition = function(slideData)
+		clearCondition = function(tutorialData, slideData)
 			local vp = tool.get_filmmaker():GetViewport()
 			if util.is_valid(vp) == false then
 				return true
 			end
 			return vp:IsSceneCamera()
 		end,
-		nextSlide = "viewport_playback_controls",
+		nextSlide = "playback_controls",
 	})
 
-	elTut:RegisterSlide("viewport_playback_controls", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("playback_controls", {
+		init = function(tutorialData, slideData, slide)
 			slide:SetFocusElement(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
-			slide:AddHighlight(slide:FindElementByPath("pfm_primary_viewport/playback_controls"))
-			slide:AddMessageBox(
-				"These are the playback controls. Here you can view your animation frame-by-frame, or in real-time by pressing the play-button. Some keybindings are also available:\n"
-					.. pfm.get_key_binding("pfm_action toggle_play")
-					.. ": Toggle Play\n"
-					.. pfm.get_key_binding("pfm_action previous_frame")
-					.. ": Previous Frame\n"
-					.. pfm.get_key_binding("pfm_action next_frame")
-					.. ": Next Frame\n"
-					.. pfm.get_key_binding("pfm_action previous_bookmark")
-					.. ": Previous Bookmark\n"
-					.. pfm.get_key_binding("pfm_action next_bookmark")
-					.. ": Next Bookmark\n"
-					.. pfm.get_key_binding("pfm_action create_bookmark")
-					.. ": Create Bookmark\n\n"
-					.. "Press the play button to continue."
-			)
+			slide:AddHighlight(slide:FindElementByPath("window_primary_viewport/playback_controls"))
+			slide:AddGenericMessageBox({
+				pfm.get_key_binding("pfm_action toggle_play"),
+				pfm.get_key_binding("pfm_action previous_frame"),
+				pfm.get_key_binding("pfm_action next_frame"),
+				pfm.get_key_binding("pfm_action previous_bookmark"),
+				pfm.get_key_binding("pfm_action next_bookmark"),
+				pfm.get_key_binding("pfm_action create_bookmark"),
+			})
 		end,
-		clearCondition = function(slideData)
+		clearCondition = function(tutorialData, slideData)
 			local vp = pm:GetViewport()
 			if util.is_valid(vp) == false then
 				return true
 			end
 			return vp:GetPlayState() == gui.PFMPlayButton.STATE_PLAYING
 		end,
-		nextSlide = "viewport_fin",
+		nextSlide = "fin",
 	})
 
-	elTut:RegisterSlide("viewport_fin", {
-		init = function(slideData, slide)
-			slide:AddMessageBox(
-				"This concludes the tutorial on the viewport.\n\n"
-					.. "You can now end the tutorial, or press the continue-button to start the next tutorial in this series."
-			)
+	elTut:RegisterSlide("fin", {
+		init = function(tutorialData, slideData, slide)
+			slide:SetTutorialCompleted()
+			slide:AddGenericMessageBox()
 		end,
-		nextSlide = "viewport_next_tutorial",
+		nextSlide = "next_tutorial",
 	})
 
-	elTut:RegisterSlide("viewport_next_tutorial", {
-		init = function(slideData, slide)
+	elTut:RegisterSlide("next_tutorial", {
+		init = function(tutorialData, slideData, slide)
 			pm:LoadTutorial("interface/render")
 		end,
 	})
-
-	elTut:StartSlide("viewport_transform_space")
-	--elTut:StartSlide("viewport_fin")
+	elTut:StartSlide("intro")
 end)
