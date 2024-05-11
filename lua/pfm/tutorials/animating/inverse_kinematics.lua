c18e0c64c51d5617fa1b6a4d54f864b7b3965f97
Silverlan <silverlann@gmail.com>
2024-05-11 21:04:29 +0200

    feat(character_posing): various general improvements

--- lua/pfm/tutorials/animating/inverse_kinematics.lua
+++ lua/pfm/tutorials/animating/inverse_kinematics.lua
@@ -10,22 +10,120 @@ include("/pfm/pfm_core_tutorials.lua")
 
 local TUT_HAND_BONE = "J_Bip_L_Hand"
 local TUT_HAND_IK_CHAIN_LENGTH = 4
-local TUT_ACTOR_UUID = "180f36ee-f747-4b85-80df-290ad4afc2ef"
-local TUT_IK_RIG = "tut_demo_ik_rig"
-local TUT_IK_HAND_TARGET_LOCATION = Vector(-112.117, 55.6728, -88.7924)
 
+
+local function get_character_target()
+	local ent, c = ents.citerator(ents.COMPONENT_IK_SOLVER)()
+	if ent == nil then
+		return
+	end
+	return ent
+end
+
+local function get_target_uuid()
+	local entTgt = get_character_target()
+	if util.is_valid(entTgt) == false then
+		return ""
+	end
+	return tostring(entTgt:GetUuid())
+end
 
 gui.Tutorial.register_tutorial("inverse_kinematics", "tutorials/animating/inverse_kinematics", function(elTut, pm)
+	elTut:SetAudioEnabled(false) -- Disabled until audio has been generated for final tutorial version
 	elTut:RegisterSlide("intro", {
 		init = function(tutorialData, slideData, slide)
 			slide:GoToWindow("actor_editor")
 
 			slide:AddGenericMessageBox()
 		end,
-		nextSlide = "options",
+		nextSlide = "place_character",
 	})
 
-	elTut:RegisterSlide("options", {
+	elTut:RegisterSlide("place_character", {
+		init = function(tutorialData, slideData, slide)
+			local window = slide:GoToWindow("model_catalog")
+			local explorer = util.is_valid(window) and window:GetExplorer() or nil
+			if util.is_valid(explorer) then
+				explorer:SetPath("characters/")
+				explorer:Update()
+			end
+
+			slide:SetFocusElement(slide:FindElementByPath("contents"))
+			slide:AddHighlight(
+				pfm.WINDOW_MODEL_CATALOG_UI_ID .. "/models_characters_generic_anime_char_male.pmdl_b",
+				true
+			)
+			slide:AddGenericMessageBox()
+		end,
+		clearCondition = function(tutorialData, slideData)
+			return get_character_target() ~= nil
+		end,
+		nextSlide = "move_tool",
+	})
+
+	elTut:RegisterSlide("move_tool", {
+		init = function(tutorialData, slideData, slide)
+			slide:AddHighlight(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID .. "/manip_controls/manip_move", true)
+
+			slide:AddGenericMessageBox()
+		end,
+		clearCondition = function(tutorialData, slideData)
+			local vp = tool.get_filmmaker():GetViewport()
+			if util.is_valid(vp) == false then
+				return true
+			end
+			return vp:GetManipulatorMode() == gui.PFMViewport.MANIPULATOR_MODE_MOVE
+		end,
+		nextSlide = "ik_controls",
+	})
+
+	elTut:RegisterSlide("ik_controls", {
+		init = function(tutorialData, slideData, slide)
+			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
+			slide:AddGenericMessageBox()
+		end,
+		nextSlide = "ik_control_types",
+	})
+
+	elTut:RegisterSlide("ik_control_types", {
+		init = function(tutorialData, slideData, slide)
+			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID))
+			slide:AddHighlight(pfm.WINDOW_PRIMARY_VIEWPORT_UI_ID .. "/manip_controls/manip_rotate", true)
+			slide:AddGenericMessageBox()
+		end,
+		nextSlide = "elbow_control",
+	})
+
+	elTut:RegisterSlide("elbow_control", {
+		init = function(tutorialData, slideData, slide)
+			slide:GoToWindow("actor_editor")
+
+			local charTarget = get_character_target()
+			if util.is_valid(charTarget) then
+				local uuidChar = tostring(charTarget:GetUuid())
+
+				slide:SetFocusElement(slide:FindElementByPath("contents"))
+				slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/actors/header")
+				slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. uuidChar .. "/header")
+				slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. uuidChar .. "/ik_solver/header")
+				slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. uuidChar .. "/ik_solver/control/header")
+				slide:AddHighlight(
+					pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. uuidChar .. "/ik_solver/control/J_Bip_L_LowerArm/header"
+				)
+				slide:AddHighlight(
+					pfm.WINDOW_ACTOR_EDITOR_UI_ID
+						.. "/"
+						.. uuidChar
+						.. "/ik_solver/control/J_Bip_L_LowerArm/contents_wrapper/strength/header"
+				)
+				slide:AddHighlight("property_controls/control_J_Bip_L_LowerArm_strength", true)
+			end
+			slide:AddGenericMessageBox()
+		end,
+		nextSlide = "quick_ik",
+	})
+
+	elTut:RegisterSlide("quick_ik", {
 		init = function(tutorialData, slideData, slide)
 			slide:AddGenericMessageBox()
 		end,
@@ -37,7 +135,22 @@ gui.Tutorial.register_tutorial("inverse_kinematics", "tutorials/animating/invers
 			slide:GoToWindow("actor_editor")
 			slide:SetFocusElement(slide:FindElementByPath("contents"))
 
-			local actor = pfm.dereference(TUT_ACTOR_UUID)
+			local vp = tool.get_filmmaker():GetViewport()
+			if util.is_valid(vp) then
+				vp:SetManipulatorMode(gui.PFMViewport.MANIPULATOR_MODE_SELECT)
+			end
+
+			local charTarget = get_character_target()
+			if util.is_valid(charTarget) then
+				local actorC = charTarget:GetComponent(ents.COMPONENT_PFM_ACTOR)
+				local actor = (actorC ~= nil) and actorC:GetActorData() or nil
+				if actor ~= nil then
+					local cmd = pfm.create_command("delete_component", actor, "ik_solver")
+					cmd:Execute()
+				end
+			end
+
+			local actor = pfm.dereference(get_target_uuid())
 			local ent = (actor ~= nil) and actor:FindEntity() or nil
 			local mdl = util.is_valid(ent) and ent:GetModel() or nil
 			local boneNames = {}
@@ -53,7 +166,7 @@ gui.Tutorial.register_tutorial("inverse_kinematics", "tutorials/animating/invers
 			local bonePath = ""
 			local itemNames = {
 				"actors",
-				TUT_ACTOR_UUID,
+				get_target_uuid(),
 				"animated",
 				"bone",
 			}
@@ -72,7 +185,7 @@ gui.Tutorial.register_tutorial("inverse_kinematics", "tutorials/animating/invers
 		end,
 		clearCondition = function(tutorialData, slideData, slide)
 			if util.is_valid(slideData.itemHandBone) == false then
-				slideData.itemHandBone = slide:FindElementByPath(TUT_ACTOR_UUID .. "/" .. TUT_HAND_BONE, false)
+				slideData.itemHandBone = slide:FindElementByPath(get_target_uuid() .. "/" .. TUT_HAND_BONE, false)
 			end
 			if util.is_valid(slideData.itemHandBone) == false then
 				return false
@@ -87,7 +200,7 @@ gui.Tutorial.register_tutorial("inverse_kinematics", "tutorials/animating/invers
 			slide:GoToWindow("actor_editor")
 			slide:SetFocusElement(slide:FindElementByPath("editor_frame/window_actor_editor"))
 			slide:AddHighlight(slide:FindElementByPath(pfm.WINDOW_ACTOR_EDITOR_UI_ID))
-			slide:AddHighlight(TUT_ACTOR_UUID .. "/" .. TUT_HAND_BONE .. "/header")
+			slide:AddHighlight(get_target_uuid() .. "/" .. TUT_HAND_BONE .. "/header")
 			slide:AddHighlight("context_menu")
 			slide:AddHighlight("context_menu/add_ik_control")
 			slide:AddHighlight("context_menu_add_ik_control/ik_control_chain_4", true)
@@ -95,23 +208,23 @@ gui.Tutorial.register_tutorial("inverse_kinematics", "tutorials/animating/invers
 			slide:AddGenericMessageBox({ TUT_HAND_IK_CHAIN_LENGTH })
 		end,
 		clearCondition = function(tutorialData, slideData, slide)
-			return util.is_valid(slide:FindElementByPath(TUT_ACTOR_UUID .. "/ik_solver/header", false))
+			return util.is_valid(slide:FindElementByPath(get_target_uuid() .. "/ik_solver/header", false))
 		end,
 		nextSlide = "controls",
-		--autoContinue = true
+		autoContinue = true,
 	})
 
 	elTut:RegisterSlide("controls", {
 		init = function(tutorialData, slideData, slide)
 			slide:GoToWindow("actor_editor")
 			slide:SetFocusElement(slide:FindElementByPath("contents"))
-			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. TUT_ACTOR_UUID .. "/header")
-			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. TUT_ACTOR_UUID .. "/ik_solver/header")
-			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. TUT_ACTOR_UUID .. "/ik_solver/control/header")
+			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. get_target_uuid() .. "/header")
+			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. get_target_uuid() .. "/ik_solver/header")
+			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. get_target_uuid() .. "/ik_solver/control/header")
 			slide:AddHighlight(
 				pfm.WINDOW_ACTOR_EDITOR_UI_ID
 					.. "/"
-					.. TUT_ACTOR_UUID
+					.. get_target_uuid()
 					.. "/ik_solver/control/"
 					.. TUT_HAND_BONE
 					.. "/header",
@@ -119,8 +232,8 @@ gui.Tutorial.register_tutorial("inverse_kinematics", "tutorials/animating/invers
 			)
 			slide:AddGenericMessageBox()
 
-			local targetInfo = slide:AddViewportTarget(TUT_IK_HAND_TARGET_LOCATION, function()
-				local ent = ents.find_by_uuid(TUT_ACTOR_UUID)
+			--[[local targetInfo = slide:AddViewportTarget(TUT_IK_HAND_TARGET_LOCATION, function()
+				local ent = ents.find_by_uuid(get_target_uuid())
 				local ikSolverC = (ent ~= nil) and ent:GetComponent(ents.COMPONENT_IK_SOLVER) or nil
 				local idx = (ikSolverC ~= nil) and ikSolverC:GetMemberIndex("control/" .. TUT_HAND_BONE .. "/position")
 					or nil
@@ -128,11 +241,11 @@ gui.Tutorial.register_tutorial("inverse_kinematics", "tutorials/animating/invers
 					or nil
 				return posDriven or Vector()
 			end, 4.0)
-			slideData.targetInfo = targetInfo
+			slideData.targetInfo = targetInfo]]
 		end,
-		clearCondition = function(tutorialData, slideData, slide)
+		--[[clearCondition = function(tutorialData, slideData, slide)
 			return slideData.targetInfo.isInRange()
-		end,
+		end,]]
 		nextSlide = "controls2",
 	})
 
@@ -140,114 +253,17 @@ gui.Tutorial.register_tutorial("inverse_kinematics", "tutorials/animating/invers
 		init = function(tutorialData, slideData, slide)
 			slide:GoToWindow("actor_editor")
 			slide:SetFocusElement(slide:FindElementByPath("contents"))
-			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. TUT_ACTOR_UUID .. "/header")
-			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. TUT_ACTOR_UUID .. "/ik_solver/header")
-			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. TUT_ACTOR_UUID .. "/ik_solver/control/header")
+			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. get_target_uuid() .. "/header")
+			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. get_target_uuid() .. "/ik_solver/header")
+			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. get_target_uuid() .. "/ik_solver/control/header")
 			slide:AddHighlight(
 				pfm.WINDOW_ACTOR_EDITOR_UI_ID
 					.. "/"
-					.. TUT_ACTOR_UUID
+					.. get_target_uuid()
 					.. "/ik_solver/control/"
 					.. TUT_HAND_BONE
 					.. "/header"
 			)
-			slide:AddGenericMessageBox()
-
-			local targetInfo = slide:AddViewportTarget(Vector(), function()
-				local ent = ents.find_by_uuid(TUT_ACTOR_UUID)
-				local ikSolverC = (ent ~= nil) and ent:GetComponent(ents.COMPONENT_IK_SOLVER) or nil
-				local idx = (ikSolverC ~= nil) and ikSolverC:GetMemberIndex("control/" .. TUT_HAND_BONE .. "/position")
-					or nil
-				local posDriven = (idx ~= nil) and ikSolverC:GetTransformMemberPos(idx, math.COORDINATE_SPACE_WORLD)
-					or nil
-				return posDriven or Vector()
-			end, 4.0)
-			slideData.targetInfo = targetInfo
-		end,
-		nextSlide = "ik_rig",
-	})
-
-	elTut:RegisterSlide("ik_rig", {
-		init = function(tutorialData, slideData, slide)
-			slide:AddGenericMessageBox()
-		end,
-		nextSlide = "ik_rig_property",
-	})
-
-	elTut:RegisterSlide("ik_rig_property", {
-		init = function(tutorialData, slideData, slide)
-			slide:GoToWindow("actor_editor")
-			slide:SetFocusElement(slide:FindElementByPath("contents"))
-
-			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. TUT_ACTOR_UUID .. "/header")
-			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. TUT_ACTOR_UUID .. "/ik_solver/header")
-			slide:AddHighlight(
-				pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. TUT_ACTOR_UUID .. "/ik_solver/base_properties/header"
-			)
-			slide:AddHighlight(
-				pfm.WINDOW_ACTOR_EDITOR_UI_ID
-					.. "/"
-					.. TUT_ACTOR_UUID
-					.. "/ik_solver/base_properties/rigConfigFile/header"
-			)
-			slide:AddHighlight(
-
-				pfm.WINDOW_ACTOR_EDITOR_UI_ID
-					.. "/"
-					.. TUT_ACTOR_UUID
-					.. "/ik_solver/base_properties/rigConfigFile/header",
-				true
-			)
-
-			slide:AddGenericMessageBox()
-		end,
-		clearCondition = function(tutorialData, slideData, slide)
-			if util.is_valid(slideData.itemRigConfigFile) == false then
-				slideData.itemRigConfigFile =
-					slide:FindElementByPath(TUT_ACTOR_UUID .. "/ik_solver/base_properties/rigConfigFile", false)
-			end
-			if util.is_valid(slideData.itemRigConfigFile) == false then
-				return false
-			end
-			return slideData.itemRigConfigFile:IsSelected()
-		end,
-		nextSlide = "ik_rig_file",
-	})
-
-	elTut:RegisterSlide("ik_rig_file", {
-		init = function(tutorialData, slideData, slide)
-			slide:GoToWindow("actor_editor")
-			slide:SetFocusElement(slide:FindElementByPath("contents"))
-
-			slide:AddHighlight(
-				pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/base_property_controls/rigConfigFile/browse_button",
-				true
-			)
-
-			slide:AddGenericMessageBox({ TUT_IK_RIG })
-		end,
-		clearCondition = function(tutorialData, slideData, slide)
-			local actor = pfm.dereference(TUT_ACTOR_UUID)
-			if actor == nil then
-				return true
-			end
-			local val = actor:GetMemberValue("ec/ik_solver/rigConfigFile")
-			return val ~= nil and #val > 0
-		end,
-		nextSlide = "ik_rig_hand_control",
-	})
-
-	elTut:RegisterSlide("ik_rig_hand_control", {
-		init = function(tutorialData, slideData, slide)
-			slide:GoToWindow("actor_editor")
-			slide:SetFocusElement(slide:FindElementByPath("contents"))
-			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. TUT_ACTOR_UUID .. "/header")
-			slide:AddHighlight(pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. TUT_ACTOR_UUID .. "/ik_solver/header")
-			slide:AddHighlight(
-				pfm.WINDOW_ACTOR_EDITOR_UI_ID .. "/" .. TUT_ACTOR_UUID .. "/ik_solver/control/header",
-				true
-			)
-
 			slide:AddGenericMessageBox()
 		end,
 		nextSlide = "conclusion",
